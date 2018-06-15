07b7b639a9b8dddd82f0f28613cf65b59736d47b
Иван Иванов <mister.herobin@gmail.com>
2018-06-15 06:15:51 +0300

    ehhh

---
+++ Common/Models/ViewModels/ProductPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using identity1.Common.Entities;
+
+
+namespace identity1.Common.Models.ViewModels
+{
+    public class ProductPage
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public string Decsription { get; set; }
+        public List<ImageOfProduct> Images { get; set; }
+        public decimal Cost { get; set; }
+        public int Count { get; set; }
+
+    }
+}
+
